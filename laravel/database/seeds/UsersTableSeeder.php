722798994fbd545d7e650378fc623cddc8284234
zloyleva <zloyleva.imt@gmail.com>
2019-07-13 18:02:10 +0300

    add simple user

--- laravel/database/seeds/UsersTableSeeder.php
+++ laravel/database/seeds/UsersTableSeeder.php
@@ -12,6 +12,10 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        factory(User::class)->create([
+            'email' => 'test@test.com',
+            'password' => bcrypt('123456789'),
+        ]);
         factory(User::class, 10)->create();
     }
 }

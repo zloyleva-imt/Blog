1adb10f26bc150d6de880c08308f4eb6ab4bf6d6
zloyleva <zloyleva.imt@gmail.com>
2019-07-27 17:52:42 +0300

    create show post for user

--- laravel/resources/views/posts/show.blade.php
+++ laravel/resources/views/posts/show.blade.php
@@ -2,4 +2,21 @@
 
 @section('content')
 
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>{{ $post->title }}</h1>
+            </div>
+        </div>
+        <div class="row">
+
+            <div class="col-12">
+
+                {!! $post->body !!}
+
+            </div>
+
+        </div>
+    </div>
+
 @endsection

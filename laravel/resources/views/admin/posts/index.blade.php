92e4a589a243d871a019ac97d2ead032bf62cf1c
zloyleva <zloyleva.imt@gmail.com>
2019-07-27 17:36:52 +0300

    create Admin layouts

--- laravel/resources/views/admin/posts/index.blade.php
+++ laravel/resources/views/admin/posts/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.admin')
 
 @section('content')
     <div class="container">
@@ -9,8 +9,14 @@
         </div>
 
         <div class="row">
+            <div class="col-12 mb-2">
+                <a class="btn btn-info" href="{{ route('admin.posts.create') }}">Create new post</a>
+            </div>
+        </div>
 
-            <form>
+        <div class="row">
+
+            <form class="col-12">
                 <div class="form-row align-items-center">
                     <div class="col-auto">
                         <input type="text" class="form-control mb-2" id="inlineFormInput"
@@ -26,39 +32,40 @@
 
         <div class="row">
 
-            <table class="table table-striped">
-                <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Title</th>
-                    <th scope="col">Status</th>
-                    <th scope="col">Author</th>
-                    <th scope="col">Date</th>
-                    <th scope="col">Action</th>
-                </tr>
-                </thead>
-                <tbody>
-
-                @foreach($posts as $post)
+            <div class="col-12">
+                <table class="table table-striped">
+                    <thead>
                     <tr>
-                        <th scope="row">{{ $loop->index + 1 + ($posts->perPage() * ($posts->currentPage() - 1)) }}</th>
-                        <td>{{ $post->title }}</td>
-                        <td>{{ $post->published_status }}</td>
-                        <td>{{ $post->user->name }}</td>
-                        <td>{{ $post->created_at }}</td>
-                        <td>
-                            <a href="{{ route('admin.posts.edit',[$post->slug]) }}">Edit</a>
-                            <form action="{{ route('admin.posts.destroy',[$post->slug]) }}" method="POST">
-                                @method('DELETE')
-                                @csrf
-                                <button>Delete</button>
-                            </form>
-                        </td>
+                        <th scope="col">#</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Author</th>
+                        <th scope="col">Date</th>
+                        <th scope="col">Action</th>
                     </tr>
-                @endforeach
+                    </thead>
+                    <tbody>
 
-            </table>
+                    @foreach($posts as $post)
+                        <tr>
+                            <th scope="row">{{ $loop->index + 1 + ($posts->perPage() * ($posts->currentPage() - 1)) }}</th>
+                            <td>{{ $post->title }}</td>
+                            <td>{{ $post->published_status }}</td>
+                            <td>{{ $post->user->name }}</td>
+                            <td>{{ $post->created_at }}</td>
+                            <td>
+                                <a href="{{ route('admin.posts.edit',[$post->slug]) }}">Edit</a>
+                                <form action="{{ route('admin.posts.destroy',[$post->slug]) }}" method="POST">
+                                    @method('DELETE')
+                                    @csrf
+                                    <button>Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
 
+                </table>
+            </div>
 
         </div>
         <div class="row">
